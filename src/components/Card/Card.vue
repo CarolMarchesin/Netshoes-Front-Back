5a653c7fb10a1408f4c9414bfef76f338b3833b1
CarolMarchesin <carol_marchesin@hotmail.com>
2025-09-03 12:42:53 -0300

    feat: add Card and CardProduct components for product display

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card" :style="{ width: width, height: height }">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+withDefaults(defineProps<{
+  width?: string;
+  height?: string;
+}>(), {
+  width: '180px',
+  height: '230px'
+});
+
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  padding: 10px;
+  box-shadow: 4px 2px 7px rgba(0, 0, 0, 0.5);
+}
+</style>
---
+++ src/components/CardProduct/CardProduct.vue
@@ -0,0 +1,43 @@
+<template>
+    <Card>
+        <template #default>
+            <div class="content-image">
+                <img :src="image" alt="Product Image" class="image" />
+            </div>
+
+            <div class="title">{{ title }}</div>
+
+        </template>
+    </Card>
+</template>
+
+<script setup lang="ts">
+import Card from '../Card/Card.vue';
+
+defineProps<{
+  title: string;
+  image: string;
+}>();
+
+</script>
+
+<style lang="scss" scoped>
+.content-image {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+}
+.image {
+    width: 8rem;
+    height: auto;
+}
+
+.title {
+    font-weight: bold;
+    font-size: 12px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+</style>
