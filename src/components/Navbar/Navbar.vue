1ea377be33f87b474b82e13ab5d86d0108d6f0ce
CarolMarchesin <carol_marchesin@hotmail.com>
2025-09-04 21:45:22 -0300

    feat: refactor Navbar component to use dynamic links and improve props structure

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,41 +1,65 @@
 <template>
     <nav class="nav">
         <ul>
-            <li><router-link :to="link">{{title}}</router-link></li>
+            <li v-for="value in links" :key="value.id">
+                <router-link :to="value.link">{{ value.title }}</router-link>
+            </li>
         </ul>
     </nav>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
 
-defineProps({
-    title: String,
-    link: String,
-});
+export interface NavbarLink {
+    id: number;
+    title: string;
+    link: string;
+}
+
+export interface NavbarProps {
+    links: NavbarLink[];
+}
+
+defineProps<NavbarProps>();
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .nav {
     padding: 20px 0px;
     border-bottom: 1px solid #999898;
+    display: flex;
 
     ul {
         list-style: none;
         padding: 0;
         margin: 0;
         display: flex;
-    }
 
-    li {
-        margin-right: 16px;
-        font-size: 14px;
-        color: #30006b;
-        font-weight: bold;
+        li {
+            margin-right: 5px;
+            font-size: 14px;
+            color: $secondary-color;
+        }
+
+        li::after {
+            content: "/";
+            color: $secondary-color;
+            margin-left: 5px;
+        }
+
+        li:last-child {
+            font-weight: bold;
+        }
+
+        li:last-child::after {
+            content: "";
+            margin: 0;
+        }
+
     }
-}
-.nav a {
-    text-decoration: none;
+    a {
+        text-decoration: none;
+    }
 }
 </style>
