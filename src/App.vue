7892bcbaa1e996e8dd9ff241068739979694f756
CarolMarchesin <carol_marchesin@hotmail.com>
2025-09-04 21:46:44 -0300

    feat: restructure App.vue to include Navbar and enhance routing links

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <template>
-  <Header></Header> 
-  <router-view></router-view>
+  <Header />
+
+  <div class="container">
+    <Navbar :links="links" />
+    <router-view />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
 import Header from './components/Header/Header.vue';
-</script>
+import Navbar from './components/Navbar/Navbar.vue';
+import { ref, watch } from 'vue';
 
+const router = useRoute();
+let links = ref([
+  { id: 0, title: 'Home', link: '/' }
+]);
+
+watch(router, (to) => {
+  if (to.path !== '/') {
+    links.value = [
+      { id: 0, title: 'Home', link: '/' },
+      { id: 1, title: String(to.name || to.path), link: to.path }
+    ];
+  } else {
+    links.value = [
+      { id: 0, title: 'Home', link: '/' }
+    ];
+  }
+});
+</script>
